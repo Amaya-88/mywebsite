09a84df350db3c88f89eb89de259d59db318c57c
amaya <azulverdosoff@hotmail.com>
2016-06-24 08:22:44 +0200

    enviroment

--- util/enviroment.php
+++ util/enviroment.php
@@ -6,4 +6,11 @@
     function isProd() {
         return strrpos($_SERVER['HTTP_HOST'], "localhost") == false;
     }
+
+    if(isProd()) {
+        ini_set('display_errors', 'off');
+        ini_set('log_errors', 'on');
+    }
+    error_reporting(E_STRICT);
+
 ?>
